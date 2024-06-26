cd0b156442349f25384dce8a6ea907127d3f54fd
Fahad Alsaigh <Fd.alsaigh@gmail.com>
2024-06-26 07:13:31 +0300

    added API URL in Urls.py

--- djangoapi/lectures/urls.py
+++ djangoapi/lectures/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from . import views
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('lecture',views.LectureView)
 
 urlpatterns = [
-    
+    path('', include(router.urls) ),
 ]
